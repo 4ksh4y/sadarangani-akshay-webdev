f2da3fd61d64b85abb3e5199d6143d3f32118a00
4kshay <4kshay@ccs.neu.edu>
2017-01-26 15:05:26 -0500

    assignment 2 progress

--- public/mean-todo/app.js
+++ public/mean-todo/app.js
@@ -5,12 +5,48 @@ angular
     .module("TodoApp", [])
     .controller("TodoController", TodoController)
 
-function TodoController($scope) {
-    $scope.todos = [
-    {'title': 'Todo 1', 'note': 'Note1'},
-    {'title': 'Todo 2', 'note': 'Note2'},
-    {'title': 'Todo 3', 'note': 'Note3'},
-    {'title': 'Todo 4', 'note': 'Note4'},
-    {'title': 'Todo 5', 'note': 'Note5'}
-    ];
+function TodoController($scope, $http) {
+    $scope.todos = [];
+
+    $http.get('/todo')
+        .success(function(response){
+            console.log(response);
+            $scope.todos = response;
+        });
+
+    $scope.createTodo = createTodo;
+    $scope.deleteTodo = deleteTodo;
+    $scope.selectTodo = selectTodo;
+    $scope.updateTodo = updateTodo;
+    $scope.selectedIndex = -1;
+    $scope.todo = {};
+
+    function createTodo(todo) {
+        console.log(todo);
+        var newTodo = {
+            title: todo.title,
+            note: todo.note
+        };
+        $scope.todos.push(newTodo);
+    }
+
+    function deleteTodo(todo) {
+        console.log(todo);
+        var index = $scope.todos.indexOf(todo);
+        console.log(index);
+        $scope.todos.splice(index, 1);
+    }
+
+    function updateTodo(todo) {
+        $scope.todos[$scope.selectedIndex].title = todo.title;
+        $scope.todos[$scope.selectedIndex].note = todo.note;
+        $scope.todo = {};
+    }
+
+    function selectTodo(todo) {
+
+        $scope.selectedIndex = $scope.todos.indexOf(todo);
+        $scope.todo.title = todo.title;
+        $scope.todo.note = todo.note;
+    }
 }
